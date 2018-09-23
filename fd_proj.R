34c9c9fd251e4e5dd58dab8db63c0a6013b04f2f
Aaron Miles <amiles2233@gmail.com>
2018-09-23 09:16:10 -0400

    Fix DraftKings to match salaries better. Add Fanduel Script

--- fd_proj.R
+++ fd_proj.R
@@ -1,11 +1,12 @@
 library(ffanalytics)
 library(lpSolve)
 
+### Week 1
 scrape <- scrape_data(src = c("ESPN", "FantasyPros", "FantasySharks", "CBS",
                               "NumberFire", "Yahoo", "NFL"),
                       pos=c('QB', 'RB', 'WR', 'TE', 'DST'),
                       season = 2018, 
-                      week=3)
+                      week=2)
 
 
 #saveRDS(scrape, 'wk1scrape.RDS')
@@ -13,16 +14,16 @@ scrape <- scrape_data(src = c("ESPN", "FantasyPros", "FantasySharks", "CBS",
 scoring <- list(
   pass = list(
     pass_att = 0, pass_comp = 0, pass_inc = 0, pass_yds = 0.04, pass_tds = 4,
-    pass_int = -1, pass_40_yds = 0,  pass_300_yds = 3, pass_350_yds = 0,
+    pass_int = -1, pass_40_yds = 0,  pass_300_yds = 0, pass_350_yds = 0,
     pass_400_yds = 0
   ),
   rush = list(
     all_pos = TRUE,
     rush_yds = 0.1,  rush_att = 0, rush_40_yds = 0, rush_tds = 6,
-    rush_100_yds = 3, rush_150_yds = 0, rush_200_yds = 0),
+    rush_100_yds = 0, rush_150_yds = 0, rush_200_yds = 0),
   rec = list(
     all_pos = TRUE,
-    rec = 1, rec_yds = 0.1, rec_tds = 6, rec_40_yds = 0, rec_100_yds = 3,
+    rec = .5, rec_yds = 0.1, rec_tds = 6, rec_40_yds = 0, rec_100_yds = 0,
     rec_150_yds = 0, rec_200_yds = 0
   ),
   misc = list(
@@ -32,7 +33,7 @@ scoring <- list(
   ),
   ret = list(
     all_pos = TRUE,
-    return_tds = 6, return_yds = 0
+    return_tds = 0, return_yds = 0
   ),
   dst = list(
     dst_fum_rec = 2,  dst_int = 2, dst_safety = 2, dst_sacks = 1, dst_td = 6,
@@ -60,12 +61,13 @@ proj <- projections_table(scrape, scoring_rules = scoring) %>%
 ## Make Optimum Lineup
 #proj <- readRDS('wk1proj.RDS')
 
-sal <- read_csv('DKSalaries.csv')
+sal <- read_csv('FDSalaries.csv')
 
 pred_sal <- proj %>% 
   filter(avg_type=='robust') %>%
-  mutate(Name = ifelse(pos=="DST", last_name, paste(first_name, last_name))) %>%
-  inner_join(sal, by=c("Name")) %>%
+  #mutate(Name = ifelse(pos=="DST", last_name, paste(first_name, last_name))) %>%
+  mutate(Name = paste(first_name, last_name)) %>%
+  inner_join(sal, by=c("Name"="Nickname")) %>%
   select(Name, team, position, points, Salary) %>%
   filter(!is.na(points), !is.na(Salary)) %>%
   group_by(Name) %>%
@@ -79,7 +81,7 @@ obj <- pred_sal$points
 mat <- rbind(t(model.matrix(~ position + 0,pred_sal)), rep(1, nrow(pred_sal)), pred_sal$Salary)
 
 dir <- c("=","=","<=","=","<=", "=","<=")
-rhs <- c(1,1,3,1,4,9,50000)
+rhs <- c(1,1,3,1,4,9,60000)
 
 result <- lp("max", obj, mat, dir, rhs, all.bin = TRUE)	
 
@@ -91,27 +93,5 @@ sum(results$points)
 
 arrange(results, position, -points)
 
-
-
-## ECR Model
-pred_sal1 <- pred_sal %>% filter(!is.na(pos_ecr))
-
-obj <- pred_sal1$pos_ecr
-
-
-mat <- rbind(t(model.matrix(~ position + 0,pred_sal1)), rep(1, nrow(pred_sal1)), pred_sal1$Salary)
-
-dir <- c("=","=","<=","=","<=", "=","<=")
-rhs <- c(1,1,3,1,4,9,50000)
-
-result <- lp("min", obj, mat, dir, rhs, all.bin = TRUE)	
-
-results <- pred_sal1[which(result$solution == 1),]
-
-results
-
-sum(results$points)
-
-arrange(results, position, -points)
 
 
